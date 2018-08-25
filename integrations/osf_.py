94b4657cea895eb254f8f821dc429db27a031308
Taylor Salo <tsalo006@fiu.edu>
2018-08-24 20:28:19 -0400

    Add nonfunctioning OSF integration.

--- integrations/osf_.py
+++ integrations/osf_.py
@@ -7,10 +7,10 @@ import os.path as op
 import requests
 import pandas as pd
 
-with open('/Users/tsalo/Documents/credentials/publons', 'r') as fo:
+with open('/Users/tsalo/Documents/credentials/osf', 'r') as fo:
     token = fo.read().strip()
 
-api_url = 'https://publons.com/api/v2/academic/{0}/'
+api_url = 'https://api.osf.io/v2/users/me/'
 score_keys = ['merit', 'editor_merit']
 
 
@@ -28,14 +28,17 @@ def populate_database():
     out_df = pd.DataFrame(columns=score_keys, index=usernames.keys())
     for user_orcid in usernames.keys():
         get_url = api_url.format(user_orcid)
+        print(get_url)
 
         response = requests.get(get_url,
                                 headers={'Authorization': 'Token {0}'.format(token),
                                          'Content-Type': 'application/json'})
         if response.ok:
             user_dict = response.json()
+            print(user_dict)
             scores = [user_dict.get(key) for key in score_keys]
         else:
+            print(response)
             scores = [None for _ in score_keys]
 
         out_df.loc[user_orcid] = scores
@@ -43,7 +46,7 @@ def populate_database():
     # Save to csv
     out_file = op.abspath(op.join(op.dirname(__file__),
                                   '../database/contributions/'
-                                  'publons.csv'))
+                                  'osf.csv'))
     out_df.to_csv(out_file, index_label='id')
 
 
